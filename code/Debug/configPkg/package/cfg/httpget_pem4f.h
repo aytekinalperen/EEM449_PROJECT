c26371d7c595f322b5f2ab8b90fac052876371e7
fgsonmez <73994873+fgsonmez@users.noreply.github.com>
2021-01-25 12:50:27 +0300

    CODE

---
+++ code/Debug/configPkg/package/cfg/httpget_pem4f.h
@@ -0,0 +1,28 @@
+/*
+ *  Do not modify this file; it is automatically 
+ *  generated and any modifications will be overwritten.
+ *
+ * @(#) xdc-B06
+ */
+
+#include <xdc/std.h>
+
+#include <ti/sysbios/knl/Semaphore.h>
+extern const ti_sysbios_knl_Semaphore_Handle semaphore0;
+
+#include <ti/sysbios/knl/Semaphore.h>
+extern const ti_sysbios_knl_Semaphore_Handle semaphore1;
+
+#include <ti/sysbios/knl/Mailbox.h>
+extern const ti_sysbios_knl_Mailbox_Handle mailbox0;
+
+#include <ti/sysbios/knl/Mailbox.h>
+extern const ti_sysbios_knl_Mailbox_Handle mailbox1;
+
+#include <ti/sysbios/knl/Mailbox.h>
+extern const ti_sysbios_knl_Mailbox_Handle mailbox2;
+
+extern int xdc_runtime_Startup__EXECFXN__C;
+
+extern int xdc_runtime_Startup__RESETFXN__C;
+
---
+++ code2/httpget.c
@@ -0,0 +1,457 @@
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+// XDCtools Header files
+#include <xdc/std.h>
+#include <xdc/runtime/Error.h>
+#include <xdc/runtime/System.h>
+
+/* TI-RTOS Header files */
+#include <ti/sysbios/BIOS.h>
+#include <ti/sysbios/knl/Task.h>
+#include <ti/sysbios/knl/Swi.h>
+#include <ti/sysbios/knl/Queue.h>
+#include <ti/sysbios/knl/Event.h>
+#include <ti/sysbios/knl/Idle.h>
+#include <ti/sysbios/knl/Mailbox.h>
+#include <ti/sysbios/knl/Clock.h>
+#include <ti/sysbios/knl/Semaphore.h>
+#include <ti/drivers/GPIO.h>
+#include <ti/net/http/httpcli.h>
+
+#include <stdbool.h>
+#include "inc/hw_memmap.h"
+#include "inc/hw_ints.h"
+#include "inc/hw_types.h"
+#include "driverlib/adc.h"
+#include "driverlib/gpio.h"
+#include "driverlib/sysctl.h"
+
+#include "Board.h"
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+//#define HOSTNAME          "api.openweathermap.org"
+//#define REQUEST_URI       "/data/2.5/forecast/?id=315202&APPID=b9bdaf75a7b1e96362a172ec83cb9303"
+//#define USER_AGENT        "HTTPCli (ARM; TI-RTOS)"
+#define SOCKETTEST_IP     "192.168.1.16"
+#define TASKSTACKSIZE     4096
+#define OUTGOING_PORT     5011
+#define INCOMING_PORT     5030
+
+extern Semaphore_Handle semaphore0; // posted by httpTask and pended by clientTask
+extern Mailbox_Handle mailbox0;
+char   tempstr[20];                     // temperature string
+
+
+uint32_t ADCValues[0];
+
+uint32_t TempValueC[11];
+
+Void ADCSwi(UArg arg1, UArg arg2)
+{
+    static uint32_t PE3_value;
+
+    //
+    // Wait for conversion to be completed for sequence 3
+    //
+    while(!ADCIntStatus(ADC0_BASE, 3, false));
+
+    //
+    // Clear the ADC interrupt flag for sequence 3
+    //
+    ADCIntClear(ADC0_BASE, 3);
+
+    //
+    // Read ADC Value from sequence 3
+    //
+    ADCSequenceDataGet(ADC0_BASE, 3, ADCValues);
+
+    //
+    // Port E Pin 3 is the AIN0 pin. Therefore connect PE3 pin to the line that you want to
+    // acquire. +3.3V --> 4095 and 0V --> 0
+    //
+    PE3_value = ADCValues[0]; // PE3 : Port E pin 3
+    PE3_value=(PE3_value/58);
+
+    // send the ADC PE3 values to the taskAverage()
+    //
+    Mailbox_post(mailbox0, &PE3_value, BIOS_NO_WAIT);
+}
+
+
+
+/*
+ *  ======== printError ========
+ */
+void printError(char *errString, int code)
+{
+    System_printf("Error! code = %d, desc = %s\n", code, errString);
+    BIOS_exit(code);
+}
+
+bool sendData2Server(char *serverIP, int serverPort, char *data, int size)
+{
+    int sockfd, connStat, numSend;
+    bool retval=false;
+    struct sockaddr_in serverAddr;
+
+    sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (sockfd == -1) {
+        System_printf("Socket not created");
+        close(sockfd);
+        return false;
+    }
+
+    memset(&serverAddr, 0, sizeof(serverAddr));  // clear serverAddr structure
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(serverPort);     // convert port # to network order
+    inet_pton(AF_INET, serverIP, &(serverAddr.sin_addr));
+
+    connStat = connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    if(connStat < 0) {
+        System_printf("sendData2Server::Error while connecting to server\n");
+    }
+    else {
+        numSend = send(sockfd, data, size, 0);       // send data to the server
+        if(numSend < 0) {
+            System_printf("sendData2Server::Error while sending data to server\n");
+        }
+        else {
+            retval = true;      // we successfully sent the temperature string
+        }
+    }
+    System_flush();
+    close(sockfd);
+    return retval;
+}
+
+Void clientSocketTask(UArg arg0, UArg arg1)
+{
+    while(1) {
+        // wait for the semaphore that httpTask() will signal
+        // when temperature string is retrieved from api.openweathermap.org site
+        //
+        Semaphore_pend(semaphore0, BIOS_WAIT_FOREVER);
+
+        GPIO_write(Board_LED0, 1); // turn on the LED
+
+        // connect to SocketTest program on the system with given IP/port
+        // send hello message whihc has a length of 5.
+        //
+        if(sendData2Server(SOCKETTEST_IP, OUTGOING_PORT, tempstr, strlen(tempstr))) {
+            System_printf("clientSocketTask:: Temperature is sent to the server\n");
+            System_flush();
+        }
+
+        GPIO_write(Board_LED0, 0);  // turn off the LED
+    }
+}
+
+void getTimeStr(char *str)
+{
+    // dummy get time as string function
+    // you may need to replace the time by getting time from NTP servers
+    //
+    strcpy(str, "2021-01-07 12:34:56");
+}
+
+float getTemperature(void)
+{
+    // dummy return
+    //
+    return atof(tempstr);
+}
+
+Void serverSocketTask(UArg arg0, UArg arg1)
+{
+    int serverfd, new_socket, valread, len;
+    struct sockaddr_in serverAddr, clientAddr;
+    char buffer[30];
+    char outstr[30], tmpstr[30];
+    bool quit_protocol;
+
+    serverfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (serverfd == -1) {
+        System_printf("serverSocketTask::Socket not created.. quiting the task.\n");
+        return;     // we just quit the tasks. nothing else to do.
+    }
+
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(INCOMING_PORT);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+
+    // Attaching socket to the port
+    //
+    if (bind(serverfd, (struct sockaddr *)&serverAddr,  sizeof(serverAddr))<0) {
+         System_printf("serverSocketTask::bind failed..\n");
+
+         // nothing else to do, since without bind nothing else works
+         // we need to terminate the task
+         return;
+    }
+    if (listen(serverfd, 3) < 0) {
+
+        System_printf("serverSocketTask::listen() failed\n");
+        // nothing else to do, since without bind nothing else works
+        // we need to terminate the task
+        return;
+    }
+
+    while(1) {
+
+        len = sizeof(clientAddr);
+        if ((new_socket = accept(serverfd, (struct sockaddr *)&clientAddr, &len))<0) {
+            System_printf("serverSocketTask::accept() failed\n");
+            continue;               // get back to the beginning of the while loop
+        }
+
+        System_printf("Accepted connection\n"); // IP address is in clientAddr.sin_addr
+        System_flush();
+
+        // task while loop
+        //
+        quit_protocol = false;
+        do {
+
+            // let's receive data string
+            if((valread = recv(new_socket, buffer, 10, 0))<0) {
+
+                // there is an error. Let's terminate the connection and get out of the loop
+                //
+                close(new_socket);
+                break;
+            }
+
+            // let's truncate the received string
+            //
+            buffer[10]=0;
+            if(valread<10) buffer[valread]=0;
+
+            System_printf("message received: %s\n", buffer);
+
+            if(!strcmp(buffer, "HELLO")) {
+                strcpy(outstr,"GREETINGS 200\n");
+                send(new_socket , outstr , strlen(outstr) , 0);
+                System_printf("Server <-- GREETINGS 200\n");
+            }
+            else if(!strcmp(buffer, "GETTIME")) {
+                getTimeStr(tmpstr);
+                strcpy(outstr, "OK ");
+                strcat(outstr, tmpstr);
+                strcat(outstr, "\n");
+                send(new_socket , outstr , strlen(outstr) , 0);
+            }
+            else if(!strcmp(buffer, "TEMPERATURE")) {
+                static uint32_t pe3_val, pe3_average, tot=0;
+                uint32_t newTemp;
+                    int i;
+                    int iter=0;
+
+                    for(i=0;i<5;i++) {     // 10 ADC values will be retrieved
+
+                               // wait for the mailbox until the buffer is full
+                               //
+                               Mailbox_pend(mailbox0, &pe3_val, BIOS_WAIT_FOREVER);
+
+
+                               tot += pe3_val;
+                           }
+                           pe3_average =(tot /5);
+
+                           newTemp = pe3_average;
+
+
+
+                           sprintf(outstr, "OK %5d\n", newTemp);
+                           //strcpy(outstr, pe3_average);
+                           System_flush();
+                send(new_socket , outstr , strlen(outstr) , 0);
+            }
+            else if(!strcmp(buffer, "QUIT")) {
+                quit_protocol = true;     // it will allow us to get out of while loop
+                strcpy(outstr, "BYE 200");
+                send(new_socket , outstr , strlen(outstr) , 0);
+            }
+
+        }
+        while(!quit_protocol);
+
+        System_flush();
+        close(new_socket);
+    }
+
+    close(serverfd);
+    return;
+}
+
+/*
+ *  ======== httpTask ========
+ *  Makes a HTTP GET request
+ */
+/*Void httpTask(UArg arg0, UArg arg1)
+{
+    bool moreFlag = false;
+    char data[64], *s1, *s2;
+    int ret, temp_received=0, len;
+    struct sockaddr_in addr;
+
+    HTTPCli_Struct cli;
+    HTTPCli_Field fields[3] = {
+        { HTTPStd_FIELD_NAME_HOST, HOSTNAME },
+        { HTTPStd_FIELD_NAME_USER_AGENT, USER_AGENT },
+        { NULL, NULL }
+    };
+
+    while(1) {
+
+        System_printf("Sending a HTTP GET request to '%s'\n", HOSTNAME);
+        System_flush();
+
+        HTTPCli_construct(&cli);
+
+        HTTPCli_setRequestFields(&cli, fields);
+
+        ret = HTTPCli_initSockAddr((struct sockaddr *)&addr, HOSTNAME, 0);
+        if (ret < 0) {
+            HTTPCli_destruct(&cli);
+            System_printf("httpTask: address resolution failed\n");
+            continue;
+        }
+
+        ret = HTTPCli_connect(&cli, (struct sockaddr *)&addr, 0, NULL);
+        if (ret < 0) {
+            HTTPCli_destruct(&cli);
+            System_printf("httpTask: connect failed\n");
+            continue;
+        }
+
+        ret = HTTPCli_sendRequest(&cli, HTTPStd_GET, REQUEST_URI, false);
+        if (ret < 0) {
+            HTTPCli_disconnect(&cli);
+            HTTPCli_destruct(&cli);
+            System_printf("httpTask: send failed");
+            continue;
+        }
+
+        ret = HTTPCli_getResponseStatus(&cli);
+        if (ret != HTTPStd_OK) {
+            HTTPCli_disconnect(&cli);
+            HTTPCli_destruct(&cli);
+            System_printf("httpTask: cannot get status");
+            continue;
+        }
+
+        System_printf("HTTP Response Status Code: %d\n", ret);
+
+        ret = HTTPCli_getResponseField(&cli, data, sizeof(data), &moreFlag);
+        if (ret != HTTPCli_FIELD_ID_END) {
+            HTTPCli_disconnect(&cli);
+            HTTPCli_destruct(&cli);
+            System_printf("httpTask: response field processing failed\n");
+            continue;
+        }
+
+        len = 0;
+        do {
+            ret = HTTPCli_readResponseBody(&cli, data, sizeof(data), &moreFlag);
+            if (ret < 0) {
+                HTTPCli_disconnect(&cli);
+                HTTPCli_destruct(&cli);
+                System_printf("httpTask: response body processing failed\n");
+                moreFlag = false;
+            }
+            else {
+                // string is read correctly
+                // find "temp:" string
+                //
+                s1=strstr(data, "temp");
+                if(s1) {
+                    if(temp_received) continue;     // temperature is retrieved before, continue
+                    // is s1 is not null i.e. "temp" string is found
+                    // search for comma
+                    s2=strstr(s1, ",");
+                    if(s2) {
+                        *s2=0;                      // put end of string
+                        strcpy(tempstr, s1+6);      // copy the string
+                        temp_received = 1;
+                    }
+                }
+            }
+
+            len += ret;     // update the total string length received so far
+        } while (moreFlag);
+
+        System_printf("Received %d bytes of payload\n", len);
+        System_printf("Temperature %s\n", tempstr);
+        System_flush();                                         // write logs to console
+
+        HTTPCli_disconnect(&cli);                               // disconnect from openweathermap
+        HTTPCli_destruct(&cli);
+        Semaphore_post(semaphore0);                             // activate socketTask
+
+        Task_sleep(5000);                                       // sleep 5 seconds
+    }
+}
+*/
+bool createTasks(void)
+{
+    static Task_Handle taskHandle2, taskHandle3;
+    Task_Params taskParams;
+    Error_Block eb;
+
+    Error_init(&eb);
+
+    Task_Params_init(&taskParams);
+    taskParams.stackSize = TASKSTACKSIZE;
+    taskParams.priority = 1;
+    taskHandle2 = Task_create((Task_FuncPtr)clientSocketTask, &taskParams, &eb);
+
+    Task_Params_init(&taskParams);
+    taskParams.stackSize = TASKSTACKSIZE;
+    taskParams.priority = 1;
+    taskHandle3 = Task_create((Task_FuncPtr)serverSocketTask, &taskParams, &eb);
+
+    if (taskHandle2 == NULL || taskHandle3 == NULL) {
+        printError("netIPAddrHook: Failed to create HTTP, Socket and Server Tasks\n", -1);
+        return false;
+    }
+
+    return true;
+}
+
+//  This function is called when IP Addr is added or deleted
+//
+void netIPAddrHook(unsigned int IPAddr, unsigned int IfIdx, unsigned int fAdd)
+{
+    // Create a HTTP task when the IP address is added
+    if (fAdd) {
+        createTasks();
+    }
+}
+
+int main(void)
+{
+    /* Call board init functions */
+    Board_initGeneral();
+    Board_initGPIO();
+    Board_initEMAC();
+
+    /* Turn on user LED */
+    GPIO_write(Board_LED0, Board_LED_ON);
+
+    System_printf("Starting the HTTP GET example\nSystem provider is set to "
+            "SysMin. Halt the target to view any SysMin contents in ROV.\n");
+    /* SysMin will only print to the console when you call flush or exit */
+    System_flush();
+
+
+    /* Start BIOS */
+    BIOS_start();
+
+    return (0);
+}
